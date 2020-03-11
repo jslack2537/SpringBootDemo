14c4849088f1b7fc7a32499ba516ded1184e7923
jslack2537 <31068641+jslack2537@users.noreply.github.com>
2020-03-11 10:47:36 -0400

    registraion controller created

---
+++ src/main/java/com/joeslack/conference/controller/RegistrationController.java
@@ -0,0 +1,16 @@
+package com.joeslack.conference.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+
+    @GetMapping("registration")
+    public String getRegistration(Map<String, Object> model) {
+
+        return "registration";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/registration.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: cjs5f
+  Date: 3/11/2020
+  Time: 10:35 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Registration</title>
+</head>
+<body>
+    <h1>Registration</h1>
+</body>
+</html>
